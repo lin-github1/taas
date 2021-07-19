6906d01167e2484872119040930cb7d82a2fce97
lin-github1 <77085593+lin-github1@users.noreply.github.com>
2021-07-19 20:17:42 +0800

    Create EditableRow.js

---
+++ src/pages/componets/EditableTable/EditableRow.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import {Form} from "antd";
+
+
+const EditableContext = React.createContext();
+
+const EditableRow = ({ index, ...props }) => {
+  const [form] = Form.useForm();
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  );
+};
+
+export default EditableRow
