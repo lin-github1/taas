e13ad11bfe4f77e9d0653663a531be19c1758474
lin-github1 <77085593+lin-github1@users.noreply.github.com>
2021-07-19 09:51:57 +0800

    Create record.js

---
+++ src/model/record.js
@@ -0,0 +1,96 @@
+import {deleteRecord, getRecordList, stopRecord, recordDetail, caseDetail} from '@/services/record';
+import moment from "moment";
+const createTimeEndValue = moment();
+const createTimeStartValue = moment().subtract(1, 'months');
+
+
+const recordModel = {
+  namespace: "record",
+  state: {
+    activeTabKey: "own",
+    taskDataList: [],
+    itemListLoading: true,
+    taskTotal: 0,
+    taskPageSize: 8,
+    taskDefaultCurrent: 1,
+    taskCurrentPage: 1,
+    timeCondition:[createTimeStartValue, createTimeEndValue],
+    taskDetailInfo: {
+      loadingData: true,
+      taskInfo: {},
+      executeResult: [],
+      CaseResultList: []
+    }
+  },
+  effects: {
+    * fetRecordList({payload}, {call, put}) {
+      const resData = yield call(getRecordList, payload);
+      const tableData = resData.MsgInfo ? resData.MsgInfo.items : [];
+      const dataTotal = resData.MsgInfo ? resData.MsgInfo.total : 0;
+      const pageNo = resData.MsgInfo ? resData.MsgInfo.page_current : 1;
+      const sizes = resData.MsgInfo ? resData.MsgInfo.page_size : 8;
+      yield put({
+        type: 'storeTaskList', payload: {
+          taskList: tableData,
+          taskTotal: dataTotal,
+          loadingData: false,
+          itemListLoading: false,
+          taskCurrentPage: pageNo,
+          taskPageSize: sizes
+        }
+      });
+    },
+    * fetDeleteTask({payload, callback}, {call, put}) {
+      const resData = yield call(deleteRecord, payload);
+      callback(resData)
+    },
+    * fetStopTask({payload, callback}, {call, put}) {
+      const resData = yield call(stopRecord, payload);
+      callback(resData)
+    },
+    * getTaskDetail({payload, callback}, {call, put}) {
+      const resData = yield call(recordDetail, payload);
+      yield put({
+        type: 'updateState', payload: {
+          taskDetailInfo: {
+            loadingData: false,
+            taskInfo: resData.MsgInfo.task_info,
+            executeResult: resData.MsgInfo.general_result,
+            CaseResultList: resData.MsgInfo.run_case,
+          }
+        }
+      });
+      callback(resData)
+    },
+    * getCaseDetail({payload, callback}, {call, put}) {
+      const resData = yield call(caseDetail, payload);
+      callback(resData.MsgInfo)
+    }
+  },
+  reducers: {
+    initTaskRequest(state, action) {
+      return {
+        ...state,
+        itemListLoading: action.payload.itemListLoading,
+      };
+    },
+    storeTaskList(state, action) {
+      return {
+        ...state,
+        taskDataList: action.payload.taskList || {},
+        taskTotal: action.payload.taskTotal,
+        loadingData: action.payload.loadingData,
+        itemListLoading: action.payload.itemListLoading,
+        taskCurrentPage: action.payload.taskCurrentPage,
+        taskPageSize: action.payload.taskPageSize
+      };
+    },
+    updateState(state, action) {
+      return {
+        ...state,
+        ...action.payload
+      }
+    }
+  },
+};
+export default recordModel;
