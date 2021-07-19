b493e425b1f0e9207cac4a5cf35b87dcffcb4cab
lin-github1 <77085593+lin-github1@users.noreply.github.com>
2021-07-19 10:00:00 +0800

    Create CaseRunHistory.js

---
+++ src/pages/Runner/components/CaseRunHistory.js
@@ -0,0 +1,119 @@
+import React, {Component} from "react";
+import {Col, Divider, Row, Statistic} from "antd";
+
+
+class CaseRunHistory extends Component {
+  render() {
+    return (
+      <div>
+        <Row gutter={{ xs: 8, sm: 16, md: 24}}>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+        </Row>
+        <Divider />
+        <Row gutter={16}>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+          <Col lg={4} md={4} sm={12} xl={4} xs={24}>
+            <Statistic
+              precision={0}
+              // prefix={<Icon type="calendar" />}
+              suffix="个"
+              title="总诊断组件数"
+              value={10}
+              valueStyle={{color: '#1890ff'}}
+            />
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+}
+
+export default CaseRunHistory
