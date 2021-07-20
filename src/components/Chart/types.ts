54b1740f1d460a88cb73f875b6cab02fde43ba99
lin-github1 <77085593+lin-github1@users.noreply.github.com>
2021-07-20 09:31:36 +0800

    Create types.ts

---
+++ src/components/Chart/types.ts
@@ -0,0 +1,28 @@
+import * as G2 from '@antv/g2/src';
+
+export type Padding =
+  | string
+  | {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+}
+  | number
+  | [number, number, number, number]
+  | [string, string];
+
+export interface LineProps {
+  top?: boolean;
+  start?: object | Array<any> | ((xScale?: any, yScale?: any) => any);
+  end?: object | Array<any> | ((xScale?: any, yScale?: any) => any);
+  lineStyle?: G2.Styles.line;
+  text?: {
+    position?: string | number;
+    autoRotate?: boolean;
+    style?: G2.Styles.text;
+    content?: string;
+    offsetX?: number;
+    offsetY?: number;
+  }
+}
